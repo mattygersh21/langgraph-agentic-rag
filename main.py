a14ba9383ab517650584aa1d8bc38091a43ae193
mattygersh21 <dakotaseia@hotmail.com>
2026-02-02 20:12:15 -0600

    - Built and complied graph. - Invoked graph from main. - Established constants in dedicated file. - Made graph nodes importable outside of graph/nodes/ directory.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
+from pprint import pprint
 
 load_dotenv()
+
+from graph.graph import app
+
 if __name__ == "__main__":
     print("Hello Advanced RAG")
+    pprint(app.invoke(input={"question": "what is agent memory?"}))
