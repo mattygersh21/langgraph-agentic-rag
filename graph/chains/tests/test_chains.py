d0ea7daa45bb4b2bde85ff61434f75e664c34db7
mattygersh21 <dakotaseia@hotmail.com>
2026-02-05 20:21:26 -0600

    - Implemented router and conditional entry pont to graph

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -9,6 +9,7 @@ from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from graph.chains.generation import generation_chain
 from ingestion import retriever
 from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
+from graph.chains.router import question_router, RouteQuery
 
 
 def test_retrival_grader_answer_yes() -> None:
@@ -52,7 +53,6 @@ def test_hallucination_grader_answer_yes() -> None:
     )
     assert res.binary_score
 
-
 def test_hallucination_grader_answer_no() -> None:
     question = "agent memory"
     docs = retriever.invoke(question)
@@ -64,3 +64,15 @@ def test_hallucination_grader_answer_no() -> None:
         }
     )
     assert not res.binary_score
+
+def test_router_to_vectorstore() -> None:
+    question = "agent memory"
+
+    res: RouteQuery = question_router.invoke({"question": question})
+    assert res.datasource == "vectorstore"
+
+def test_router_to_websearch() -> None:
+    question = "how to make pizza"
+
+    res: RouteQuery = question_router.invoke({"question": question})
+    assert res.datasource == "websearch"
